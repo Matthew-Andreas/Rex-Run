ea66b06a079e84bbc03ddb6ff4681021e1b8db31
Adolfo Gante <adolfogante@gmail.com>
2025-03-23 00:45:12 -0700

    added button for level 5

--- scripts/level_select.gd
+++ scripts/level_select.gd
@@ -27,4 +27,8 @@ func _on_level_3_pressed() -> void:
 
 
 func _on_level_4_pressed() -> void:
-	GameManager.load_level("res://scenes/example_level.tscn")
+	GameManager.load_level("res://scenes/level_scenes/level_4.tscn")
+	
+
+func _on_level_5_pressed() -> void:
+	GameManager.load_level("res://scenes/level_scenes/level_5.tscn")
